d4deb5a517cef159eab48fc9d62f8e7b0685497f
Jmoisio1 <jaakko.moisio@student.lut.fi>
2022-03-10 18:37:47 +0200

    L07T1-T4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,9 +10,20 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.myapplication.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    TextView text;
+    EditText inputxt;
+    EditText inputxt1;
+
 
     private ActivityMainBinding binding;
 
@@ -20,18 +31,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        inputxt = findViewById(R.id.editTextTextPersonName);
+
+
+        text = findViewById(R.id.textView);
+        inputxt1 = findViewById(R.id.editTextTextPersonName);
+
+        inputxt1 = (EditText) findViewById(R.id.editTextTextPersonName);
+        //Register listener
+        inputxt1.addTextChangedListener(this);
+
     }
 
+    public void Function(View v) {
+        System.out.println("Hello World!");
+        text.setText("Hello World!");
+
+    }
+
+
+    public void inputFunction(View v) {
+        text.setText(inputxt.getText());
+    }
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        text.setText(s);
+    }
 }
